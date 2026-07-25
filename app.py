1c05c505fa2648c052cc61fefd6156ecb8f73590
Dhanush S <dhanushgowdas222@gmail.com>
2026-07-25 07:18:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,12 +151,20 @@ with right:
 # -----------------------------
 #CURRENT AI STATUS Card
 # -----------------------------
-st.markdown(f"""
-<div class='status'>
-CURRENT AI STATUS
-<br><br>
-{prediction}
-</div>
-""", unsafe_allow_html=True)
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("""
+    <div class='status'>
+    CURRENT AI STATUS
+    </div>
+    """, unsafe_allow_html=True)
+
+with col2:
+    st.markdown(f"""
+    <div class='status'>
+    {prediction}
+    </div>
+    """, unsafe_allow_html=True)
 
 
